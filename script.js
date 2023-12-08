070ffccc6763af415166b1ab6cb23376e84ea583
hjenp22 <hjipark22@gmail.com>
2023-12-08 14:53:09 -0600

    // everything out going to redo hw

--- script.js
+++ script.js
@@ -1,98 +1,99 @@
-const apiKey = "73b0cba1ba046852f98801a447debff3"; //open weather
-const city = 'destination'
+// const apiKey = "73b0cba1ba046852f98801a447debff3"; //open weather
+// const city = 'destination'
 
-const apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+// const apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-async function getWeatherData(city) {
-    const response = await fetch(`${apiUrl}?q=${city}&appid=${"73b0cba1ba046852f98801a447debff3"}`);
-    const data = await response.json();
-    return data;
-}
+// async function getWeatherData(city) {
+//     const response = await fetch(`${apiUrl}?q=${city}&appid=${"73b0cba1ba046852f98801a447debff3"}`);
+//     const data = await response.json();
+//     return data;
+// }
 
-function updateCurrentWeather(data) {
-    const currentWeatherContainer = document.getElementById('currentWeather');
+// function updateCurrentWeather(data) {
+//     const currentWeatherContainer = document.getElementById('currentWeather');
 
-    const cityName = data.name;
-    const date = new Date(data.dt * 1000);
-    const icon = data.weather[0].icon;
-    const temperature = data.main.temp;
-    const humidity = data.main.humidity;
-    const windSpeed = data.wind.speed;
-
-
-    currentWeatherContainer.innerHTML = `
-        <h2>${city}</h2>
-        <p>Date: ${date.toDateString()}</p>
-        <p>Temperature: ${temperature}°C</p>
-        <p>Humidity: ${humidity}%</p>
-        <p>Wind Speed: ${windSpeed} m/s</p>
-    `;
-}
-
-function updateForecast(data) {
-    const forecastContainer = document.getElementById('forecast');
-    const weeklyForecastContainer = document.getElementById('weeklyForecast');
-
-    forecastContainer.innerHTML = '';
-    weeklyForecastContainer.innerHTML = '<h2>Weekly Firecase</h2>';
-
-    let forecastHTML = '<h2>5-Day Forecast</h2>';
-    for (let i = 0; i < data.list.length; i += 8) { 
-        const date = new Date(data.list[i].dt * 1000);
-        const icon = data.list[i].weather.icon;
-        const temperature = data.list[i].main.temp;
-        const windSpeed = data.list[i].wind.speed;
-        const humidity = data.list[i].main.humidity;
-
-        forecastHTML += `
-            <div class="forecast-day">
-                <p>Date: ${date.toDateString()}</p>
-                <img src="https://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">
-                <p>Temperature: ${temperature}°C</p>
-                <p>Wind Speed: ${windSpeed} m/s</p>
-                <p>Humidity: ${humidity}%</p>
-            </div>
-        `;
-    }
-    forecastContainer.innerHTML = forecastHTML;
-}
-
-document.getElementById('searchForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
-    const cityInput = document.getElementById('cityInput');
-    const city = cityInput.value.trim();
-
-    if (city) {
-        const weatherData = await getWeatherData(city);
-        updateCurrentWeather(weatherData);
-        updateForecast(weatherData);
-        updateSearchHistory(city);
-    }
-});
-
-document.getElementById('searchHistory').addEventListener('click', async function (event) {
-    if (event.target.matches('button')) {
-        const city = event.target.textContent;
-        const weatherData = await getWeatherData(city);
-        updateCurrentWeather(weatherData);
-        updateForecast(weatherData);
-    }
-});
+//     const cityName = data.name;
+//     const date = new Date(data.dt * 1000);
+//     const icon = data.weather[0].icon;
+//     const temperature = data.main.temp;
+//     const humidity = data.main.humidity;
+//     const windSpeed = data.wind.speed;
 
 
-function updateSearchHistory(city) {
-    const searchHistoryContainer = document.getElementById('searchHistory');
+//     currentWeatherContainer.innerHTML = `
+//         <h2>${city}</h2>
+//         <p>Date: ${date.toDateString()}</p>
+//         <p>Temperature: ${temperature}°C</p>
+//         <p>Humidity: ${humidity}%</p>
+//         <p>Wind Speed: ${windSpeed} m/s</p>
+//     `;
+// }
+
+// function updateForecast(data) {
+//     const forecastContainer = document.getElementById('forecast');
+//     const weeklyForecastContainer = document.getElementById('weeklyForecast');
+
+//     forecastContainer.innerHTML = '';
+//     weeklyForecastContainer.innerHTML = '<h2>Weekly Firecase</h2>';
+
+//     let forecastHTML = '<h2>5-Day Forecast</h2>';
+//     for (let i = 0; i < data.list.length; i += 8) { 
+//         const date = new Date(data.list[i].dt * 1000);
+//         const icon = data.list[i].weather.icon;
+//         const temperature = data.list[i].main.temp;
+//         const windSpeed = data.list[i].wind.speed;
+//         const humidity = data.list[i].main.humidity;
+
+//         forecastHTML += `
+//             <div class="forecast-day">
+//                 <p>Date: ${date.toDateString()}</p>
+//                 <img src="https://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">
+//                 <p>Temperature: ${temperature}°C</p>
+//                 <p>Wind Speed: ${windSpeed} m/s</p>
+//                 <p>Humidity: ${humidity}%</p>
+//             </div>
+//         `;
+//     }
+//     forecastContainer.innerHTML = forecastHTML;
+// }
+
+// document.getElementById('searchForm').addEventListener('submit', async function (event) {
+//     event.preventDefault();
+//     const cityInput = document.getElementById('cityInput');
+//     const city = cityInput.value.trim();
+
+//     if (city) {
+//         const weatherData = await getWeatherData(city);
+//         updateCurrentWeather(weatherData);
+//         updateForecast(weatherData);
+//         updateSearchHistory(city);
+//     }
+// });
+
+// document.getElementById('searchHistory').addEventListener('click', async function (event) {
+//     if (event.target.matches('button')) {
+//         const city = event.target.textContent;
+//         const weatherData = await getWeatherData(city);
+//         updateCurrentWeather(weatherData);
+//         updateForecast(weatherData);
+//     }
+// });
+
+
+// function updateSearchHistory(city) {
+//     const searchHistoryContainer = document.getElementById('searchHistory');
     
 
-    const button = document.createElement('button');
-    button.textContent = city;
+//     const button = document.createElement('button');
+//     button.textContent = city;
 
-    button.addEventListener('click', async function () {
-        const weatherData = await getWeatherData(city);
-        updateCurrentWeather(weatherData);
-        updateForecast(weatherData);
-    });
+//     button.addEventListener('click', async function () {
+//         const weatherData = await getWeatherData(city);
+//         updateCurrentWeather(weatherData);
+//         updateForecast(weatherData);
+//     });
 
    
-    searchHistoryContainer.appendChild(button);
-}
+//     searchHistoryContainer.appendChild(button);
+// }
+
